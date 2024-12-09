621a8084f52a8a2745853d76c5297cb74b25581b
MAA91 <Sanya13082007@gmail.com>
2024-12-09 17:15:33 +0300

    Shedule for group done

--- SQL/DML/SQLQuery6 - AGGREGATION.sql
+++ SQL/DML/SQLQuery6 - AGGREGATION.sql
@@ -1,14 +1,10 @@
-USE AcademySQL;
+USE PD_318_DML;
 
 SELECT 
-	--COUNT(direction_id) 
-	--FORMATMESSAGE(N'%s %s %s', last_name, first_name, ISNULL(middle_name, N'')) AS N'Ф.И.О.',
-	--group_name		AS N'Группа',
 	direction_name	AS N'Направление обучения',
 	COUNT(group_id) AS N'Количество групп'
-	--COUNT(student_id) AS N'Количество студентов'
-FROM Groups, Directions--, Students
+FROM Groups, Directions
 WHERE direction = direction_id
---AND	  [group] = group_id
 GROUP BY direction_name 
+HAVING	  COUNT(group_id) > 3
 ;
